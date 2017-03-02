7f0c3cfed78f04ab908b6798e7385abe2fce7553
shiyong <shiyong@shiyongdeMac-mini.local>
2017-03-02 16:48:51 +0800

    Handling Text Input

---
+++ index.ios.js
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+    Image
+} from 'react-native';
+
+class AwesomeProject extends Component {
+    render() {
+        return (
+            <View style={{
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+                <View style={{width: 50, height: 50, backgroundColor: 'blue'}} />
+                <View style={{width: 50, height: 50, backgroundColor: 'red'}} />
+                <View style={{width: 50, height: 50, backgroundColor: 'black'}} />
+            </View>
+
+        );
+    }
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
